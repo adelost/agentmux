ad053eae24cb25d0610b79f47b7f117ec62e64a9
Mattias Wetterlind <adelost@gmail.com>
2026-07-30 20:38:11 +0200

    build(link): release current CircleKit clients

--- android/audio-inbox/app/build.gradle.kts
+++ android/audio-inbox/app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "io.agentmux.audioinbox"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.1.2"
+        versionCode = 5
+        versionName = "1.1.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
