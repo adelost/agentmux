e13b860fd55abc4daa88b282f92b5a5c50969165
Mattias Wetterlind <adelost@gmail.com>
2026-08-09 10:47:27 +0200

    build(link): release 1.2.6

--- android/audio-inbox/wear/build.gradle.kts
+++ android/audio-inbox/wear/build.gradle.kts
@@ -29,7 +29,7 @@ android {
         applicationId = "io.agentmux.audioinbox"
         minSdk = 30
         targetSdk = 35
-        versionCode = 11
+        versionCode = 12
         versionName = linkVersionName
     }
 
